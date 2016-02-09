7718068d11b35420ad59bf37fd7b449e5b9d16a7
Michal <kroko440@wp.pl>
2016-02-09 16:54:58 +0100

    Switching panels in main menu

---
+++ Assets/GUI/Scripts/SwitchPanelsButton.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SwitchPanelsButton : MonoBehaviour {
+
+	public GameObject turnOffPanel;
+	public GameObject turnOnPanel;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+
+	public void switchPanels() {
+		if (turnOnPanel && turnOffPanel) {
+			turnOffPanel.SetActive (false);
+			turnOnPanel.SetActive (true);
+		} else {
+			Debug.LogError ("SwitchPanelsButton : TurnOnPanel or TurnOffPanel not initialized!");
+		}
+	}
+}
