83892704136ebde9b81009390095467edef4eede
Michal <kroko440@wp.pl>
2016-02-21 00:35:58 +0100

    Added SurvivalSpawner with overtime-scaling difficulty. Enemies no more collide with each other. Added StarterWeaponMonster.

--- Assets/Gameplay/Scripts/Map.cs
+++ Assets/Gameplay/Scripts/Map.cs
@@ -3,8 +3,7 @@ using System.Collections;
 
 public class Map : MonoBehaviour {
 
-    [SerializeField]
-    private float xMovementRange, yMovementRange; // Player movement boundaries
+    [SerializeField] float xMovementRange, yMovementRange; // Player movement boundaries
 
     public float XMovementRange 
     {
---
+++ Assets/Gameplay/Scripts/SurvivalSpawner.cs
@@ -0,0 +1,83 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SurvivalSpawner : MonoBehaviour {
+
+    [SerializeField]  bool isActive = true;
+    [SerializeField]  float initialMonstersPerMinute = 20;
+    [SerializeField]  float MpMPerMinuteGrow = 5; // MonstersPerMinute per minute grow
+    [SerializeField]  Enemy[] enemyPrefabs;
+
+    float monstersPerMinute;
+    float timeBetweenSpawns; // in seconds
+    float lastSpawnTime;
+
+    Map map;
+
+    void Start()
+    {
+        Player.playerDeathEvent += disable;
+        map = GameObject.FindObjectOfType<Map>();
+        monstersPerMinute = initialMonstersPerMinute;
+    }
+
+    void OnDisable()
+    {
+        Player.playerDeathEvent -= disable;
+    }
+
+    void Update()
+    {
+        if (isActive && Time.timeSinceLevelLoad > lastSpawnTime + getTimeBetweenSpawns())
+        {
+            spawnEnemy();
+            lastSpawnTime = Time.timeSinceLevelLoad;
+            monstersPerMinute = getMonstersPerMinute();
+        }
+    }
+
+    void spawnEnemy()
+    {
+        Enemy enemy = (Enemy) Instantiate(enemyPrefabs[Random.Range(0,enemyPrefabs.Length)], getSpawnPosition(), transform.rotation);
+    }
+
+    Vector3 getSpawnPosition()
+    {
+        float x, y;
+        if (Random.value > 0.5f)
+        {
+            x = map.XMovementRange + 1;
+            y = Random.Range(-map.YMovementRange - 1, map.YMovementRange + 1);
+            if (Random.value > 0.5f)
+                x *= -1;
+        }
+        else
+        {
+            x = Random.Range(-map.XMovementRange - 1, map.XMovementRange + 1);
+            y = map.YMovementRange + 1;
+            if (Random.value > 0.5f)
+                y *= -1;
+        }
+        return new Vector3(x, y, 0);
+    }
+
+    float getTimeBetweenSpawns()
+    {
+        return 60/monstersPerMinute;
+    }
+    float getMonstersPerMinute()
+    {
+        if (Time.timeSinceLevelLoad / 60 > 1)
+        {
+            float secondsBetweenMpMGrow = 60 / MpMPerMinuteGrow;
+
+            return initialMonstersPerMinute + Time.timeSinceLevelLoad / 60 * MpMPerMinuteGrow;
+        }
+        return initialMonstersPerMinute;
+    }
+
+    void disable()
+    {
+        isActive = false;
+    }
+}
