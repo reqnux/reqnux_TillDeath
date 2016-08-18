7a6106ee92b9c66b81f74c78cb6ef5b692bfeed4
Michal <kroko440@wp.pl>
2016-08-18 14:27:28 +0200

    DirectToPlayerEnemyMovement added

---
+++ Assets/Gameplay/Scripts/Enemies/EnemyMovement/DirectToPlayerEnemyMovement.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class DirectToPlayerEnemyMovement : UnitMovement {
+
+	Player player;
+	Unit movingUnit;
+
+	void Start () {
+		player = GameObject.Find("Player").GetComponent<Player>();
+		movingUnit = GetComponent<Unit>();
+	}
+	
+	protected override void OnEnable()
+	{
+		base.OnEnable();
+	}
+
+	protected override void OnDisable()
+	{
+		base.OnDisable();
+	}
+
+	protected override void move() 
+	{
+		lookAtTarget(player.transform.position);
+		GetComponent<Rigidbody2D>().velocity = transform.up * movingUnit.Stats.MovementSpeed;
+	}
+}
