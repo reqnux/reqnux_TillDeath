f8ec7218aa7054885456c37119adb659b02f8ab0
Michal <kroko440@wp.pl>
2016-08-28 13:14:24 +0200

    Test scene with time control added

---
+++ Assets/TestScene/TimeControl.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TimeControl : MonoBehaviour {
+
+	[SerializeField] float scaleChange = 0.25f;
+	bool showTimeScale;
+
+
+	void Update () {
+		if (Input.GetKeyDown (KeyCode.KeypadPlus)) {
+			Time.timeScale += scaleChange;
+		}
+		
+		if (Input.GetKeyDown (KeyCode.KeypadMinus)) {
+			Time.timeScale = Mathf.Max (0f, Time.timeScale - scaleChange);
+		}
+		if (Input.GetKeyDown (KeyCode.BackQuote)) { //tylda
+			showTimeScale = !showTimeScale;
+		}
+	}
+
+	void OnGUI() {
+		if (showTimeScale) {
+			GUI.Label (new Rect (20, 100, 80, 30), Time.timeScale.ToString ());
+		}
+	}
+
+}
