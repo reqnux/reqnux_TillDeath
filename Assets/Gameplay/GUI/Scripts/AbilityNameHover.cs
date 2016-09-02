1f83a36e12d0b2487ef7990a7edb78fd4a604ff5
Michal <kroko440@wp.pl>
2016-09-02 11:15:24 +0200

    PlayerPanel base

---
+++ Assets/Gameplay/GUI/Scripts/AbilityNameHover.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.EventSystems;
+using System.Collections;
+
+public class AbilityNameHover : MonoBehaviour, IPointerEnterHandler, IPointerExitHandler {
+
+	[SerializeField] GameObject descriptionPanel;
+	[SerializeField] string abilityDescription;
+
+	public void OnPointerEnter(PointerEventData eventData)
+	{
+		descriptionPanel.SetActive (true);
+		//descriptionPanel.transform.FindChild ("Description").GetComponent<Text> ().text = abilityDescription;
+	}
+	public void OnPointerExit(PointerEventData eventData) 
+	{
+		descriptionPanel.SetActive (false);
+	}
+}
