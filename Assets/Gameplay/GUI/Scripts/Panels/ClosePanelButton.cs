987ed5f51a3f009e88f3c6a5a6ae8c67182cdadc
Michal <kroko440@wp.pl>
2016-09-09 12:25:14 +0200

    PlayerPanel opens on Tab

---
+++ Assets/Gameplay/GUI/Scripts/Panels/ClosePanelButton.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ClosePanelButton : MonoBehaviour {
+
+	[SerializeField] GameObject panel;
+
+    public void closePanel() 
+    {
+		panel.SetActive(false);
+	}
+}
--- Assets/Gameplay/Scripts/GameManager/KeyboardEvents.cs
+++ Assets/Gameplay/Scripts/GameManager/KeyboardEvents.cs
@@ -3,9 +3,12 @@ using System.Collections;
 
 public class KeyboardEvents : MonoBehaviour {
 
-    public GameObject pausePanel;
+    public GameObject playerPanel;
+	public GameObject pausePanel;
 
     void Update () {
+		if (Input.GetKeyDown(KeyCode.Tab))
+			playerPanel.SetActive(!playerPanel.activeInHierarchy);
         if (Input.GetKeyDown(KeyCode.Escape))
             pausePanel.SetActive(!pausePanel.activeInHierarchy);
     }
