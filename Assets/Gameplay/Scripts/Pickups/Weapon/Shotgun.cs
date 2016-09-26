724fa34fc3687e32c5bf5033d2fc91aea7dae882
Michal <kroko440@wp.pl>
2016-09-26 20:19:27 +0200

    AutomaticShotgun added and improved Shotgun shooting.

--- Assets/Gameplay/Scripts/Pickups/Weapon/Shotgun.cs
+++ Assets/Gameplay/Scripts/Pickups/Weapon/Shotgun.cs
@@ -3,10 +3,39 @@ using System.Collections;
 
 public class Shotgun : Weapon {
 
+	float gunEndingTranslation;
+	Vector3 gunEndingPos;
+	int randomAngle;
+
     protected override void Awake () 
     {
         base.Awake();
+		gunEndingPos = gunEnding.localPosition;
 		bulletsPerShot = 7;
 		shotConeWidth = 30;
+		randomAngle = 3;
     }
+
+	public override void shoot() {
+		if (canShoot())
+		{
+			Debug.Log ("local pos = " + gunEndingPos);
+			gunEnding.Rotate(new Vector3(0, 0, -shotConeWidth / 2));
+			for (int i = 0; i < bulletsPerShot; i++)
+			{
+				gunEndingTranslation = Random.Range (0f, 1f);
+				gunEnding.localPosition = new Vector3(gunEnding.localPosition.x , gunEnding.localPosition.y + gunEndingTranslation,0);
+				spawnBullet();
+				gunEnding.localPosition = gunEndingPos;
+				gunEnding.RotateAround(gunEnding.position, Vector3.forward, shotConeWidth / bulletsPerShot + Random.Range(-randomAngle, randomAngle));
+			}
+			gunEnding.localRotation = Quaternion.identity;
+
+			weaponSound.playShotSound();
+			lastShotTime = Time.time;
+			currentAmmo--;
+			if (currentAmmo == 0)
+				reload();
+		}
+	}
 }
