f0a48c0eeb12b17a85a51e4630cefe2b6ee97a94
Michal <kroko440@wp.pl>
2016-10-26 20:34:37 +0200

    MissionNameDisplay added

---
+++ Assets/Gameplay/Scripts/Missions/MissionNameDisplay.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+using System.Collections;
+
+public class MissionNameDisplay : MonoBehaviour {
+
+	void Awake () {
+		int missionNumber = Formatter.sceneNameToMissionNumber (SceneManager.GetActiveScene ().name);
+		GetComponent<Text> ().text = MissionsNames.names [missionNumber - 1];
+	}
+	
+}
